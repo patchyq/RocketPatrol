bd205f613377816b7929a3ce604945ec486fb126
Patrick <pqueiroz@ucsc.edu>
2022-04-27 22:29:50 -0700

    runs phaser

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 let config = {
-    type: Phaser.CANVAS,
-    width: 640,
-    height: 480
-  }
+  type: Phaser.AUTO,
+  width: 640,
+  height: 480,
+  scene: [ Menu, Play ]
+}
 
 let game = new Phaser.Game(config);
 
